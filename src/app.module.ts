914decc27514f7f1e3fb818790054810e68dca94
Mauricio Alexandre <malexandresilveira@gmail.com>
2022-03-24 19:25:18 -0300

    Start commit

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'tuffi.db.elephantsql.com',
+      port: 3306,
+      username: 'csfxpsdy',
+      password: 'h7PTvgX_8raW2nNGY2BpjjbcV_FQBjm8',
+      database: 'csfxpsdy',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
